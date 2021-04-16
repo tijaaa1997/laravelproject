cb3eccf1304b8568a2b3dd9b361efdcde5756212
Milos Tijanic <mikigrobar1997@gmail.com>
2021-04-16 20:16:19 +0200

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+       <title>cars project</title>
+    </head>
+    <body class="antialiased">
+        <h2>Welcome to the cars project</h2>
+    </body>
+</html>
